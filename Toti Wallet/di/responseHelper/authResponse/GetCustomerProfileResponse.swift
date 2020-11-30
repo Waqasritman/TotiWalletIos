4b924589e6b03921602e8bd98a57b5478b9dffbd
mohammadwaqas <waqas@yeepeey.com>
2020-11-30 12:04:34 +0400

    customeradded

---
+++ Toti Wallet/di/responseHelper/authResponse/GetCustomerProfileResponse.swift
@@ -0,0 +1,24 @@
+//
+//  GetCustomerProfileResponse.swift
+//  Toti Wallet
+//
+//  Created by Mohammad Waqas on 11/30/20.
+//  Copyright © 2020 iOS Technologies. All rights reserved.
+//
+
+import Foundation
+import XMLMapper
+
+class GetCustomerProfileResponse:XMLMappable  {
+    required init?(map: XMLMap) {
+        
+    }
+    
+    var nodeName: String!
+    
+    func mapping(map: XMLMap) {
+        
+    }
+    
+    
+}
